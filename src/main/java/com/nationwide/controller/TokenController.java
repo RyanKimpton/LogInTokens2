1edb64d85bdcd22a1af3ea2ff1f4b369421af201
Jason <no>
2019-11-28 11:50:02 +0000

    added delete functionality

--- src/main/java/com/nationwide/controller/TokenController.java
+++ src/main/java/com/nationwide/controller/TokenController.java
@@ -1,5 +1,5 @@
 package com.nationwide.controller;
-
+//test
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
